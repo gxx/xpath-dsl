dd6ea7e0a8c72388ede128471961fc5f43bbc791
Andrew Crosio <andrew.crosio@gmail.com>
2014-05-07 23:19:54 +0400

    making builder more flexible as an xpath fragment

--- xpath_dsl/builder.py
+++ xpath_dsl/builder.py
@@ -1,6 +1,9 @@
 from xpath_dsl.base import XPathBase
 from xpath_dsl.mixins import NavigationMixin
+from xpath_dsl.path import Any
 from xpath_dsl.path import Current
+from xpath_dsl.path import Parent
+from xpath_dsl.path import Root
 
 
 class Builder(XPathBase, NavigationMixin):
@@ -11,5 +14,17 @@ class Builder(XPathBase, NavigationMixin):
         return ''
 
     @property
+    def any(self):
+        return Any()
+
+    @property
     def current(self):
         return Current()
+
+    @property
+    def parent(self):
+        return Parent()
+
+    @property
+    def root(self):
+        return Root()
